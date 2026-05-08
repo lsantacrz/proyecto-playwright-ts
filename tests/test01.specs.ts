b8658d3c2664e0dde211b94022e4ca7ffa655bfd
lsantacrz <lsantacruz@impactws.com>
2026-05-08 10:11:25 -0600

    configuracion inicialt

---
+++ tests/test01.specs.ts
@@ -0,0 +1,8 @@
+import{test} from '@playwright/test';
+import{ CommonPageMethods } from '../pages/common-page/common-page.methods';
+
+
+test('Login', async ({ page }) => {
+    const commonPageMethods = new CommonPageMethods(page);
+    await commonPageMethods.navigateToTheApplication();
+});
